b5d34cb4a0377633f839565d750ddd1abc877259
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-04-15 23:41:22 +0300

    Обновление gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ require('../../gulp/gulp-init.js')({HTML: '.'});
 
 const comb = require('../../gulp/tasks/comb');
 const cs = require('../../gulp/tasks/create-structure');
-const scss = require('../../gulp/tasks/scss.js').scss;
+const { scss, scssDev } = require('../../gulp/tasks/scss.js');
 const mincss = require('../../gulp/tasks/mincss');
 const uglifyes = require('../../gulp/tasks/uglify').uglifyes;
 const { sync, syncInit } = require('../../gulp/tasks/sync');
@@ -20,4 +20,5 @@ task('cs', cs);
 task('combScss', comb);
 task('uglifyEs6', series(uglifyes, sync));
 task('sass', series(scss));
+task('sassDev', series(scssDev));
 task('watch', watchFiles);
