c5f08a1b2e4b4e7a0951339cff6dabecedb04bce
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-04-19 23:25:14 +0300

    Реализована карусель.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
+  /* menu */
   let $btnHumburger = $('.header-navbar__hamburger');
   let $navbarList = $('.header-navbar__list');
   let $window = $(window);
   let $body = $('body');
   let isShow = false;
 
-  function toggleActive() {
+  let toggleActive = () => {
     $btnHumburger.toggleClass('active');
     isShow ? $body.removeAttr('class') : $body.toggleClass('active');
     $navbarList.fadeToggle(500);
     isShow = !isShow;
-  }
+  };
 
   $btnHumburger.on('click', () => {
     toggleActive();
@@ -24,4 +25,45 @@ $(document).ready(function(){
       toggleActive();
     }
   });
+
+  /* slider */
+  let $slides = $('.slide');
+  let $indContainer = $('.indicators');
+  let $indItems = $('.indicator');
+  let currentSlide = 0;
+  let playbackStatus = true;
+  const carouselInterval = 5000;
+  const movedInterval = 500;
+
+  let gotoNSlide = (n) => {
+    const i = currentSlide;
+
+    $($slides[currentSlide]).toggleClass('active moved');
+    $($indItems[currentSlide]).toggleClass('active');
+    currentSlide = (n + $slides.length) % $slides.length;
+    $($slides[currentSlide]).toggleClass('active');
+    $($indItems[currentSlide]).toggleClass('active');
+
+    setTimeout(() => {
+      $($slides[i]).removeClass('moved');
+    }, movedInterval)
+  };
+
+  let gotoNextSlide = () => gotoNSlide(currentSlide + 1);
+
+  let slideInterval = setInterval(gotoNextSlide, carouselInterval);
+
+  let pauseSlideShow = () => {
+    if (playbackStatus) {
+      playbackStatus = !playbackStatus;
+      clearInterval(slideInterval);
+    }
+  };
+
+  let clickIndicatorBtn = (e) => {
+    pauseSlideShow();
+    gotoNSlide(+e.target.getAttribute('data-slide-to'));
+  };
+
+  $indContainer.on('click', '.indicator', clickIndicatorBtn);
 });
