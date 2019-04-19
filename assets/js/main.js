59756f8650443800562b423d5c5d73c0468de148
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-04-19 19:41:57 +0300

    Закончена адаптивность.

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  let $btnHumburger = $('.header-navbar__hamburger');
+  let $navbarList = $('.header-navbar__list');
+  let $window = $(window);
+  let $body = $('body');
+  let isShow = false;
+
+  function toggleActive() {
+    $btnHumburger.toggleClass('active');
+    isShow ? $body.removeAttr('class') : $body.toggleClass('active');
+    $navbarList.fadeToggle(500);
+    isShow = !isShow;
+  }
+
+  $btnHumburger.on('click', () => {
+    toggleActive();
+    return false;
+  });
+
+  $body.on('click', () => isShow && toggleActive());
+
+  $window.on('resize', () => {
+    if ($window.width() > 768 && isShow) {
+      toggleActive();
+      console.log(isShow);
+    }
+  });
+});
