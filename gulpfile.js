b7d5128239856a1e80a3f59bb928457e190f63ec
Alecto <fomenkoandrey1978@gmail.com>
2021-01-04 11:26:16 +0200

    grid-gap -> gap

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 'use strict';
 
-require('../../gulp/gulp-init.js')({HTML: '.'});
+require('/webProjects/gulp/gulp-init.js')({HTML: '.'});
 
-const comb = require('../../gulp/tasks/comb');
-const cs = require('../../gulp/tasks/create-structure');
-const { scss, scssDev, scssDC } = require('../../gulp/tasks/scss.js');
-const mincss = require('../../gulp/tasks/mincss');
-const uglifyes = require('../../gulp/tasks/uglify').uglifyes;
-const { sync, syncInit } = require('../../gulp/tasks/sync');
+const comb = require('/webProjects/gulp/tasks/comb.js'),
+    scssDC     = require('/webProjects/gulp/tasks/scss.js').scssDC,
+    mincss   = require('/webProjects/gulp/tasks/mincss.js'),
+    uglifyes = require('/webProjects/gulp/tasks/uglify.js').uglifyes,
+    { sync, syncInit } = require('/webProjects/gulp/tasks/sync.js');
 
 function watchFiles () {
     syncInit();
@@ -16,9 +15,7 @@ function watchFiles () {
     watch($.PATH.html.files, sync);
 }
 
-task('cs', cs);
 task('combScss', comb);
 task('uglifyEs6', series(uglifyes, sync));
 task('sass', series(scssDC));
-task('sassDev', series(scssDev));
 task('watch', watchFiles);
